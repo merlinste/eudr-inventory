a18aef8ac1ef7892fa87826cfcef56562a9f41eb
merlins <111979824+merlinste@users.noreply.github.com>
2025-09-01 21:59:14 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+class GlobalErrorBoundary extends React.Component<{children: React.ReactNode}, {error: any}> {
+  constructor(props:any){ super(props); this.state = { error: null } }
+  static getDerivedStateFromError(error:any){ return { error } }
+  componentDidCatch(error:any, info:any){ console.error('App error:', error, info) }
+  render(){
+    if (this.state.error) {
+      return (
+        <div style={{padding:16, color:'#b91c1c', background:'#fee2e2', border:'1px solid #fecaca'}}>
+          <b>Unerwarteter Fehler</b>
+          <pre style={{whiteSpace:'pre-wrap'}}>{String(this.state.error)}</pre>
+        </div>
+      )
+    }
+    return this.props.children
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <GlobalErrorBoundary>
       <App />
-    </BrowserRouter>
+    </GlobalErrorBoundary>
   </React.StrictMode>
 )
