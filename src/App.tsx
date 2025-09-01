e739a3f2ac64a932eb0f86a7e314b07191418f2c
merlins <111979824+merlinste@users.noreply.github.com>
2025-09-01 20:59:36 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,15 @@ export default function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/admin/users" element={<AdminUsers />} />
       <Route path="lots/:id" element={<LotDetail />} />
+      <Route element={<Shell/>}>
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/lots" element={<Lots/>} />
+        <Route path="/lots/:id" element={<LotDetail/>} />  {/* jetzt in der Shell */}
+        <Route path="/productions" element={<Productions/>} />
+        <Route path="/partners" element={<Partners/>} />
+        <Route path="/eudr" element={<Eudr/>} />
+</Route>
+<Route path="/login" element={<Login/>} />
       <Route
         path="/*"
         element={
